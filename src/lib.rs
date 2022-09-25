93d9165b86380f404431b88c8cd7b0503215f7c5
Károly Török <torok.karoly.krisztian@gmail.com>
2022-09-25 17:25:51 +0200

    Sample unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 mod utils;
 
-use std::fmt::format;
-
 use wasm_bindgen::prelude::*;
 
 use std::fmt;
@@ -50,6 +48,30 @@ impl Universe {
             }
         }
         count
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+    
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
@@ -106,11 +128,12 @@ impl Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
+        utils::set_panic_hook();
         let width = 128;
         let height = 128;
 
         // Initial state of the universe
-        let cells = (0..width * height).map(|i| {
+        let cells = (0..width * height).map(|_i| {
             if js_sys::Math::random() < 0.5 {
                 Cell::Alive
             } else {
